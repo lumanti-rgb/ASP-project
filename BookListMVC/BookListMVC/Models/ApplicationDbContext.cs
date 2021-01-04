3d1157124770a01dc9ddf1a3108fb93816f03bb8
lumanti-rgb <lumantimanandhar091@gmail.com>
2021-01-04 16:41:10 +0545

    first commit

---
+++ BookListMVC/BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext :DbContext
+    { 
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
